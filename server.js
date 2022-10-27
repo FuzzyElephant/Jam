4bf4ee581c6b8def60487acd09a935e46cb7901d
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2022-10-26 21:45:45 -0400

    Server replicates new players to all clients and passes movement info along

--- server.js
+++ server.js
@@ -1,45 +1,98 @@
 var uuid = require('uuid-random');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.WebSocketServer({port:8080}, ()=> { console.log('Server Started')})
+const wss = new WebSocket.WebSocketServer({ port: 8080 }, () => { console.log('Server Started') })
 
-var playerData = { "type" : "PlayerData" }
+//var playerData = { "type": "PlayerData" }
+var playerData = {}
 
 function isString(x) {
     return Object.prototype.toString.call(x) === "[object String]"
 }
 
+function BroadcastMsgToClients(msg, clientToSkip) {
+
+    console.log(`BroadcastMsgToClients() -- ${msg}`)
+    for (player in playerData) {
+        if (player === clientToSkip) {
+            console.log(`   Skipping -- ${player}`)
+            continue;
+        }
+
+        console.log(`       Sending to ${player}`)
+        var currentClient = playerData[player]
+        currentClient["Handle"].send(msg)
+    }
+}
+
 //=====WEBSOCKET FUNCTIONS======
 
 //Websocket function that managages connection with clients
-wss.on('connection', function connection(client){
+wss.on('connection', function connection(client) {
 
     //Create Unique User ID for player
     client.id = uuid();
 
     console.log(`Client ${client.id} Connected!`)
 
-    var currentClient = playerData[""+client.id]
+    playerData["" + client.id] = new Object();
+    var currentClient = playerData["" + client.id]
+    currentClient["Handle"] = client;
 
-    //Send default client data back to client for reference
-    client.send(`{"id": "${client.id}"}`)
+    currentClient["Handle"].send(`{"cmd": "addPlayer", "id": "${client.id}", "isLocalPlayer": "true"}`)
+    BroadcastMsgToClients(`{"cmd": "addPlayer", "id": "${client.id}", "isLocalPlayer": "false"}`, client.id)
+
+    for (player in playerData) {
+        if (player == client.id) {
+            continue
+        }
+
+        currentClient["Handle"].send(`{"cmd": "addPlayer", "id": "${player}", "isLocalPlayer": "false"}`)
+    }
+
+  /*  for (player in playerData) {
+
+       // if (player["Handle"] != null)
+        console.log(`handling ${player}`)
+        var curClient = playerData[player]
+        if (player == client.id) {
+            curClient["Handle"].send(`{"id": "${client.id}", "isLocalPlayer": "true"}`)
+        }
+        else {
+            curClient["Handle"].send(`{"id": "${client.id}","isLocalPlayer": "false"}`)
+
+        }
+    }*/
 
     //Method retrieves message from client
     client.on('message', (data) => {
+      //  console.log(`Message recvd = ${data}`)
+    //    console.log(`type of is ${typeof data}`)
+        {
 
-        if (isString(data)) {
+            try {
 
-            var dataJSON = JSON.parse(data)
+                var dataJSON = JSON.parse(data)
+                if (dataJSON.cmd != null && dataJSON.cmd === 'updatePos') {
+                    console.log(`Updating position for ${client.id}`)
+                    BroadcastMsgToClients(JSON.stringify(dataJSON), client.id);
 
-            console.log("Player Message")
-            console.log(dataJSON)
+                }
+              //  console.log(`Player Message ----_> "${dataJSON.xPos}`)
+              //  console.log(dataJSON)
+
+            } catch (e) {
+
+            }
         }
+
     })
 
     //Method notifies when client disconnects
     client.on('close', () => {
-	    console.log('This Connection Closed!')
-    console.log("Removing Client: " + client.id)
+        console.log('This Connection Closed!')
+        console.log("Removing Client: " + client.id)
+        currentClient.client = null
     })
 
 })
